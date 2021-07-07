98c72e18fa268a14ed04f3b6bf2be536e3df60de
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2021-07-08 00:40:22 +0200

    unifying sample production scripts

--- SampleProduction/hadronizeEvents.py
+++ SampleProduction/hadronizeEvents.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 
+# regular mass points
 mX_min = 400
 #mX_min = 1400
 #mX_min = 3800
@@ -11,25 +12,55 @@ mX_max = 4000
 mX_step = 200
 mY_step = 200
 
-
 failedMassPoints = []
 #failedMassPoints = [[1600, 500], [3000, 260], [3000, 300], [3000, 500], [3000, 700], [3000, 900], [3000, 1100], [3000, 1300]]
+
+# benchmark points
+massPoints_BP = ['M3_1600_M2_500_BPb', 'M3_2000_M2_300_BPd', 'M3_2000_M2_800_BPe', 'M3_2500_M2_300_BPf']
+failedMassPoints_BP = []
 
 sleep_minutes = 18
 
 
-def processMassPoint(mX, mY,counter):
+from optparse import OptionParser
+parser = OptionParser()
+
+parser.add_option('--bp', dest="bp", action='store_true',
+                  help="Process TRSM benchmark points",
+                  default=False)
+
+(options, args) = parser.parse_args()
+
+
+if not options.bp:
+    failedMassPoints_str = []
+    for mp in failedMassPoints:
+        failedMassPoints_str.append('M3_%i_M2_%i' % (mp[0],mp[1]))
+    failedMassPoints = failedMassPoints_str
+else:
+    failedMassPoints = failedMassPoints_BP
+
+massPoints = []
+if not options.bp:
+    # loop over mass points
+    for mX in range(mX_min, mX_max + mX_step, mX_step):
+        for mY in ([260] + range(300, mX-125, mY_step)):
+            massPoints.append('M3_%i_M2_%i' % (mX,mY))
+else:
+    massPoints = massPoints_BP
+
+
+def processMassPoint(massPoint,counter):
     print ('---------------------------------------')
-    print ('(mX, mY) = (%i, %i)' % (mX, mY))
+    print ('mass point = %s' % massPoint)
     print ('---------------------------------------')
 
     # create jdl file
-    jdl_path = os.path.join(condor_folder, ('TRSM_XToHY_6b_M3_%i_M2_%i.jdl' % (mX, mY)))
+    jdl_path = os.path.join(condor_folder, ('TRSM_XToHY_6b_%s.jdl' % massPoint))
     jdl_file = open(jdl_path,'w')
     jdl_content = jdl_template
     jdl_content = re.sub('DUMMY_OUTPUTDIR',condor_folder,jdl_content)
-    jdl_content = re.sub('M3MASS',str(mX),jdl_content)
-    jdl_content = re.sub('M2MASS',str(mY),jdl_content)
+    jdl_content = re.sub('DUMMY_MASSPOINT',massPoint,jdl_content)
     jdl_file.write(jdl_content)
     jdl_file.close()
 
@@ -45,18 +76,17 @@ def processMassPoint(mX, mY,counter):
 jdl_template = """universe = vanilla
 Notification = never
 Executable = DUMMY_OUTPUTDIR/hadronizeEvents.sh
-Output = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_M3_M3MASS_M2_M2MASS.stdout
-Error = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_M3_M3MASS_M2_M2MASS.stderr
-Log = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_M3_M3MASS_M2_M2MASS.log
-Arguments = M3MASS M2MASS
+Output = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_DUMMY_MASSPOINT.stdout
+Error = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_DUMMY_MASSPOINT.stderr
+Log = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_DUMMY_MASSPOINT.log
+Arguments = DUMMY_MASSPOINT
 Queue
 """
 
 
 bash_template = """#!/bin/bash
 
-M3MASS=$1
-M2MASS=$2
+MASSPOINT=$1
 
 START_TIME=`/bin/date`
 echo "Started at $START_TIME"
@@ -73,7 +103,7 @@ wget https://raw.githubusercontent.com/cms-sw/genproductions/3aacccd15d69b1ba71a
 scram b
 cd ../..
 
-cmsDriver.py Configuration/GenProduction/python/Hadronizer_TuneCP5_13TeV_generic_LHE_pythia8_noPSweights_cff.py --mc --eventcontent RAWSIM --datatier GEN --conditions 106X_mc2017_realistic_v6 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN --geometry DB:Extended --era Run2_2017 --customise Configuration/DataProcessing/Utils.addMonitoring --filein file:/STORE/ferencek/TRSM_XToHY_6b/2017/13TeV/LHE/TRSM_XToHY_6b_M3_${M3MASS}_M2_${M2MASS}.lhe --fileout file:/STORE/ferencek/TRSM_XToHY_6b/2017/13TeV/GEN/TRSM_XToHY_6b_M3_${M3MASS}_M2_${M2MASS}_GEN.root --python_filename config.py -n -1 --no_exec
+cmsDriver.py Configuration/GenProduction/python/Hadronizer_TuneCP5_13TeV_generic_LHE_pythia8_noPSweights_cff.py --mc --eventcontent RAWSIM --datatier GEN --conditions 106X_mc2017_realistic_v6 --beamspot Realistic25ns13TeVEarly2017Collision --step GEN --geometry DB:Extended --era Run2_2017 --customise Configuration/DataProcessing/Utils.addMonitoring --filein file:/STORE/ferencek/TRSM_XToHY_6b/2017/13TeV/LHE/TRSM_XToHY_6b_${MASSPOINT}.lhe --fileout file:/STORE/ferencek/TRSM_XToHY_6b/2017/13TeV/GEN/TRSM_XToHY_6b_${MASSPOINT}_GEN.root --python_filename config.py -n -1 --no_exec
 
 sed -i '34,37d' config.py
 sed -i '32d' config.py
@@ -89,6 +119,8 @@ exit $exitcode
 
 
 condor_folder = 'condor_hadronize_events'
+if options.bp:
+    condor_folder += '_BP'
 os.system('mkdir -p ' + condor_folder)
 
 # create Bash script
@@ -103,12 +135,10 @@ counter = 0
 if len(failedMassPoints):
     for mp in failedMassPoints:
         counter += 1
-        processMassPoint(mp[0],mp[1],counter)
+        processMassPoint(mp,counter)
 else:
-    # loop over mass points
-    for mX in range(mX_min, mX_max + mX_step, mX_step):
-        for mY in ([260] + range(300, mX-125, mY_step)):
-            counter += 1
-            processMassPoint(mX,mY,counter)
+    for mp in massPoints:
+        counter += 1
+        processMassPoint(mp,counter)
 
 print ('---------------------------------------')
--- SampleProduction/runDelphes.py
+++ SampleProduction/runDelphes.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 
+# regular mass points
 mX_min = 400
 #mX_min = 600
 #mX_max = 400
@@ -9,25 +10,55 @@ mX_max = 4000
 mX_step = 200
 mY_step = 200
 
-
 failedMassPoints = []
 #failedMassPoints = [[3200, 2700], [3200, 2900], [3400, 260], [3400, 1100], [3400, 1300], [3400, 1500], [3600, 1900], [3600, 2100], [3600, 2300], [3600, 2500], [3600, 2700], [3600, 2900], [3800, 900], [3800, 1300], [3800, 1500], [4000, 1500], [4000, 1700]]
+
+# benchmark points
+massPoints_BP = ['M3_1600_M2_500_BPb', 'M3_2000_M2_300_BPd', 'M3_2000_M2_800_BPe', 'M3_2500_M2_300_BPf']
+failedMassPoints_BP = []
 
 sleep_minutes = 10
 
 
-def processMassPoint(mX, mY,counter):
+from optparse import OptionParser
+parser = OptionParser()
+
+parser.add_option('--bp', dest="bp", action='store_true',
+                  help="Process TRSM benchmark points",
+                  default=False)
+
+(options, args) = parser.parse_args()
+
+
+if not options.bp:
+    failedMassPoints_str = []
+    for mp in failedMassPoints:
+        failedMassPoints_str.append('M3_%i_M2_%i' % (mp[0],mp[1]))
+    failedMassPoints = failedMassPoints_str
+else:
+    failedMassPoints = failedMassPoints_BP
+
+massPoints = []
+if not options.bp:
+    # loop over mass points
+    for mX in range(mX_min, mX_max + mX_step, mX_step):
+        for mY in ([260] + range(300, mX-125, mY_step)):
+            massPoints.append('M3_%i_M2_%i' % (mX,mY))
+else:
+    massPoints = massPoints_BP
+
+
+def processMassPoint(massPoint,counter):
     print ('---------------------------------------')
-    print ('(mX, mY) = (%i, %i)' % (mX, mY))
+    print ('mass point = %s' % massPoint)
     print ('---------------------------------------')
 
     # create jdl file
-    jdl_path = os.path.join(condor_folder, ('TRSM_XToHY_6b_M3_%i_M2_%i.jdl' % (mX, mY)))
+    jdl_path = os.path.join(condor_folder, ('TRSM_XToHY_6b_%s.jdl' % massPoint))
     jdl_file = open(jdl_path,'w')
     jdl_content = jdl_template
     jdl_content = re.sub('DUMMY_OUTPUTDIR',condor_folder,jdl_content)
-    jdl_content = re.sub('M3MASS',str(mX),jdl_content)
-    jdl_content = re.sub('M2MASS',str(mY),jdl_content)
+    jdl_content = re.sub('DUMMY_MASSPOINT',massPoint,jdl_content)
     jdl_file.write(jdl_content)
     jdl_file.close()
 
@@ -44,18 +75,17 @@ jdl_template = """universe = vanilla
 Notification = never
 #Requirements = (OpSysAndVer == "CentOS7" && machine != "lorientree02.hep.zef.irb.hr" )
 Executable = DUMMY_OUTPUTDIR/runDelphes.sh
-Output = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_M3_M3MASS_M2_M2MASS.stdout
-Error = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_M3_M3MASS_M2_M2MASS.stderr
-Log = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_M3_M3MASS_M2_M2MASS.log
-Arguments = M3MASS M2MASS
+Output = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_DUMMY_MASSPOINT.stdout
+Error = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_DUMMY_MASSPOINT.stderr
+Log = DUMMY_OUTPUTDIR/TRSM_XToHY_6b_DUMMY_MASSPOINT.log
+Arguments = DUMMY_MASSPOINT
 Queue
 """
 
 
 bash_template = """#!/bin/bash
 
-M3MASS=$1
-M2MASS=$2
+MASSPOINT=$1
 
 START_TIME=`/bin/date`
 echo "Started at $START_TIME"
@@ -79,7 +109,7 @@ ROOTCFLAGS=$(root-config --cflags --libs)
 sed -i 's/-std=c++0x //g' Makefile
 make -j 6
 
-./DelphesCMSFWLite cards/delphes_card_CMS.tcl /STORE/ferencek/TRSM_XToHY_6b/2017/13TeV/DelphesCMS/TRSM_XToHY_6b_M3_${M3MASS}_M2_${M2MASS}_DelphesCMS.root /STORE/ferencek/TRSM_XToHY_6b/2017/13TeV/GEN/TRSM_XToHY_6b_M3_${M3MASS}_M2_${M2MASS}_GEN.root
+./DelphesCMSFWLite cards/delphes_card_CMS.tcl /STORE/ferencek/TRSM_XToHY_6b/2017/13TeV/DelphesCMS/TRSM_XToHY_6b_${MASSPOINT}_DelphesCMS.root /STORE/ferencek/TRSM_XToHY_6b/2017/13TeV/GEN/TRSM_XToHY_6b_${MASSPOINT}_GEN.root
 exitcode=$?
 
 echo ""
@@ -90,6 +120,8 @@ exit $exitcode
 
 
 condor_folder = 'condor_delphes'
+if options.bp:
+    condor_folder += '_BP'
 os.system('mkdir -p ' + condor_folder)
 
 # create Bash script
@@ -104,12 +136,10 @@ counter = 0
 if len(failedMassPoints):
     for mp in failedMassPoints:
         counter += 1
-        processMassPoint(mp[0],mp[1],counter)
+        processMassPoint(mp,counter)
 else:
-    # loop over mass points
-    for mX in range(mX_min, mX_max + mX_step, mX_step):
-        for mY in ([260] + range(300, mX-125, mY_step)):
-            counter += 1
-            processMassPoint(mX,mY,counter)
+    for mp in massPoints:
+        counter += 1
+        processMassPoint(mp,counter)
 
 print ('---------------------------------------')
