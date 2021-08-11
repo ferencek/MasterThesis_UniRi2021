51644c8cc2c563bb63eb69ae8e2bb02842c9816f
Lara Calic <lcalic@cern.ch>
2021-08-11 16:28:02 +0200

    updated makePlots.py (buggy)

--- Analysis/makePlots.py
+++ Analysis/makePlots.py
@@ -1,8 +1,8 @@
 import ROOT as r
 import copy
 
-#---------------------------------------------------------------------
-def function (graph, name):
+#--------------------------------------------------------------------------------------------------------------------------------
+def function (graph,graphs_BP, name):
 
     #############################
     # The "surface" drawing options ("surf", "col", "cont") impose their own internal coordinate system
@@ -126,8 +126,6 @@ n = 0
 for mX in range(mX_min, mX_max + mX_step, mX_step):
     for mY in ([260] + range(300, mX-125, mY_step)):
         f = r.TFile("/users/lcalic/nobackup/cmsdas_2020_gen/condor_analysis_jobs_20210802145153/HISTOGRAMS_TRSM_XToHY_6b_M3_%i_M2_%i.root" % (mX, mY))
-        #f = r.TFile("/users/lcalic/nobackup/cmsdas_2020_gen/condor_analysis_jobs_GenJetNoNu_20210602123957/HISTOGRAMS_TRSM_XToHY_6b_M3_%i_M2_%i.root" % (mX, mY))
-        #f = r.TFile("/users/ferencek/TRSM_analysis/condor_analysis_jobs_GenJetNoNu_20210610132757/HISTOGRAMS_TRSM_XToHY_6b_M3_%i_M2_%i.root" % (mX, mY))
         f.cd()
         h1_b = f.Get("h_multiplicityN_higgs_candidates")
         h2_b = f.Get('h_DeltaR_bb_vs_higgspt')
@@ -150,11 +148,57 @@ for mX in range(mX_min, mX_max + mX_step, mX_step):
             boosted_higgs_graphsGenJet[count].SetPoint(n, mX, mY, frac_genjet)
         n += 1
         
-function (gr_gen, "BoostedHiggsFraction_gen.pdf")
-function (gr_genjet, "BoostedHiggsFraction_genjet.pdf")
+        
+#BP lists         
+points = [(1600, 500),(2000, 300),(2000, 800),(2500, 300)]
+suffix = ["BPb","BPd", "BPe","BPf"]
+gr_gen_BP = [r.TGraph2D() for point in points]
+gr_genjet_BP = [r.TGraph2D() for point in points]
+boosted_higgs_graphsGen_BP = [[r.TGraph2D() for point in points] for i in range (4)]
+boosted_higgs_graphsGenJet_BP = [[r.TGraph2D() for point in points] for i in range (4)]
+
+
+for p in range(len(points)):
+ 
+    gr_gen_BP[p].SetTitle(";m_{X} [GeV];m_{Y} [GeV];Fraction of boosted Higgs boson candidates")
+    gr_genjet_BP[p].SetTitle(";m_{X} [GeV];m_{Y} [GeV];Fraction of boosted Higgs boson candidates")
+    for i in range(4):
+        boosted_higgs_graphsGen_BP[i][p].SetTitle(";m_{X} [GeV];m_{Y} [GeV];Event selection eff. (%i H cand.)"%i)
+        boosted_higgs_graphsGenJet_BP[i][p].SetTitle(";m_{X} [GeV];m_{Y} [GeV];Event selection eff. (%i H cand.)"%i)
+        
+
+for p, (mX,mY) in enumerate(points):
+        n=0
+        f = r.TFile("/users/lcalic/nobackup/cmsdas_2020_gen/HISTOGRAMS_TRSM_XToHY_6b_M3_%i_M2_%i_%s.root" % (mX, mY, suffix[p]))
+        f.cd()
+        h1_b = f.Get("h_multiplicityN_higgs_candidates")
+        h2_b = f.Get('h_DeltaR_bb_vs_higgspt')
+        h2_n = f.Get('h_higgs_pt_all')
+        h1_n = f.Get('h_multiplicityN_higgs_candidates_boosted')
+        frac_gen = h2_b.Integral(0,h2_b.GetNbinsX()+1,0,h2_b.GetYaxis().FindBin(0.8)-1)/ h2_n.Integral(0,h2_n.GetNbinsX()+1)
+        nHiggsCands=0
+        for i in range(1,5):
+            nHiggsCands += h1_b.GetBinContent(i+1)*i
+        frac_genjet=float(nHiggsCands)/h2_n.Integral(0,h2_n.GetNbinsX()+1)
+        print ("(mX, mY) = (%i, %i)" % (mX, mY))
+        print (frac_gen)
+        print (frac_genjet)
+        gr_gen_BP[p].SetPoint(n,mX,mY,frac_gen)
+        gr_genjet_BP[p].SetPoint(n,mX,mY,frac_genjet)
+        for count in range(4):
+            frac_gen = h1_n.GetBinContent(count+1) / h1_n.Integral()
+            frac_genjet = h1_b.GetBinContent(count+1) / h1_b.Integral()
+            boosted_higgs_graphsGen_BP[count][p].SetPoint(n, mX, mY, frac_gen)
+            boosted_higgs_graphsGenJet_BP[count][p].SetPoint(n, mX, mY, frac_genjet)
+        
+
+
+
+function (gr_gen,gr_gen_BP,  "BoostedHiggsFraction_gen.pdf")
+function (gr_genjet, gr_genjet_BP, "BoostedHiggsFraction_genjet.pdf")
 for i in range (4):  
-   function(boosted_higgs_graphsGen[i], "Event_Selection_eff_%i_gen.pdf"%i)
-   function(boosted_higgs_graphsGenJet[i],"Event_Selection_eff_%i_genJet.pdf"%i) 
+   function(boosted_higgs_graphsGen[i], boosted_higgs_graphsGen_BP[i],"Event_Selection_eff_%i_gen.pdf"%i)
+   function(boosted_higgs_graphsGenJet[i],boosted_higgs_graphsGenJet_BP[i],"Event_Selection_eff_%i_genJet.pdf"%i) 
     
 
 
