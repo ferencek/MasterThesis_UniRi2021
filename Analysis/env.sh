eea4e5b3ef9c64a9144f700539219001eb9a9b21
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2021-08-31 10:47:05 +0200

    removed Delphes from the environment setup

--- Analysis/env.sh
+++ Analysis/env.sh
@@ -2,7 +2,7 @@ BASE_DIR=`pwd`
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd /users/ferencek/CMSDAS_2020/MCGen/CMSSW_10_6_5/
 cmsenv
-cd /users/ferencek/CMSDAS_2020/MCGen/Delphes-3.5.0/
-source DelphesEnv.sh
-export ROOT_INCLUDE_PATH=`pwd`/external:$ROOT_INCLUDE_PATH
+#cd /users/ferencek/CMSDAS_2020/MCGen/Delphes-3.5.0/
+#source DelphesEnv.sh
+#export ROOT_INCLUDE_PATH=`pwd`/external:$ROOT_INCLUDE_PATH
 cd $BASE_DIR
